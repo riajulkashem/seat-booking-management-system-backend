dc06eb7fd7e6755b9b61c954a1f34cbba484c9c3
Riajul Kashem <riajul.programmer@gmail.com>
2024-08-29 02:22:07 +0600

    lint

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
