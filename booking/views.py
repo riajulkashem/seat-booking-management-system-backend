2ea09d5631bd153db4d020084a1fb3ced13d939f
Riajul Kashem <riajul.programmer@gmail.com>
2024-08-28 14:43:02 +0600

    create viewsets for handling CRUD operations

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from typing import Any, Dict
+from rest_framework.request import Request
+from rest_framework import viewsets
+from .models import Venue, Seat, Booking
+from .serializers import VenueSerializer, SeatSerializer, BookingSerializer
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class VenueViewSet(viewsets.ModelViewSet):
+    queryset = Venue.objects.all()
+    serializer_class = VenueSerializer
+
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def create(
+        self, request: Request, *args: Any, **kwargs: Dict[str, Any]
+    ) -> Response:
+        # Check if the seat is already booked at the requested date and time
+        seat_id: str = request.data.get("seat", "")
+        booking_date: str = request.data.get("booking_date", "")
+        booking_time: str = request.data.get("booking_time", "")
+
+        if Booking.objects.filter(
+            seat_id=seat_id, booking_date=booking_date, booking_time=booking_time
+        ).exists():
+            return Response(
+                {"error": "Seat is already booked at this time"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return super().create(request, *args, **kwargs)
